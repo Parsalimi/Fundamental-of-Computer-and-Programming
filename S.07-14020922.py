698b148f870dec4d974bffc1e1a46ae795e0989a
Parsa Salimi <salimi1382@gmail.com>
2023-12-16 23:46:59 +0330

    Update S07

--- S.07-14020922.py
+++ S.07-14020922.py
@@ -1,4 +1,5 @@
 ################## Lecture ##################
+#برنامه ای بنویسید که عدد صحیح و مثبت n را از ورودی دریافت کند وعبارت (...+!3+!2+!1)
 # n = int(input("Enter the no: "))
 # f = 1
 # sum = 0
@@ -6,6 +7,7 @@
 #     f *= k
 #     sum += k
 # print(sum)
+
 #######################################
 #Barnamei benevisid ke adad sahih va mosbat n ra az vorodi daryaft konad va majmoeh argham aan adad ra be sorat khoroji namayesh bede
 # n = int(input("Please enter a number: "))
@@ -14,18 +16,28 @@
 #     s += n % 10
 #     n //= 10
 # print(s)
+
 #######################################
 #Barnamei Benevisid x ra az khoroji daryaft konad va y ra mohasebe kone va print kone
 # x > 0     y = x^2-7x+13
 # x = 0     y = 5
 # x < 0     y = 3x-6
-x = int(input("Enter the no: "))
-if x==0:
-    y=5
-elif x > 0:
-    y = x*x-7*x+13
-else:
-    y = 3*x+6
-print(y)
+# x = int(input("Enter the no: "))
+# if x==0:
+#     y=5
+# elif x > 0:
+#     y = x*x-7*x+13
+# else:
+#     y = 3*x+6
+# print(y)
+
+#######################################
+#برنامه ای بنویسید که عبارت زیر را نمایش دهد
+
+#1
+#1 2
+#1 2 3
+#1 2 3 4
+
 #edit by AIconstructor
 ################## Assignment ##################

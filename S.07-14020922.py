b323f037559acfd31e60443f55ddb96e8f05be10
Parsa Salimi <salimi1382@gmail.com>
2023-12-17 00:29:48 +0330

    Update S.07-14020922.py

--- S.07-14020922.py
+++ S.07-14020922.py
@@ -28,11 +28,17 @@ else:
     y = 3*x+6
 print(y)
 
+#برنامه ای بنویسید که عبارت زیر را نمایش دهد
+
+#1
+#1 2
+#1 2 3
+#1 2 3 4
+a=int(input("input the number: "))
+for i in range (1,a+1):
+    for j in range (1,i+1):
+        print (j,"\t",end='')
+        #salam be srcs
+#edited by emaan.
 ################## Assignment ##################
-# سلام خوشگلا
-# slm
-# damet garm doorood bar to
-# Slm test shomare 2
-# mashti mashala
-# Are didam Hossein
-# eyval
+

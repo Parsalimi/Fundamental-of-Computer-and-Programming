8593c221127edd9736df88aba6246e967fb21ec0
Parsa Salimi <salimi1382@gmail.com>
2024-01-12 16:16:12 +0330

    Exam Prep Added

--- S.05-14020902.py
+++ S.05-14020902.py
@@ -1,28 +1,28 @@
 ################## Lecture ##################
 
 ################## Assignment ##################
-print("Tamrin #1")
-a = int(input("Adad aval ro vared konid: "))
-b = int(input("Adad dovom ro vared konid: "))
-print("Jam 2 adad mishavad: {}".format(a+b))
+# print("Tamrin #1")
+# a = int(input("Adad aval ro vared konid: "))
+# b = int(input("Adad dovom ro vared konid: "))
+# print("Jam 2 adad mishavad: {}".format(a+b))
 
-print("Tamrin #2")
-a = int(input("Adad aval ro vared konid: "))
-b = int(input("Adad dovom ro vared konid: "))
-print("Jam 2 adad mishavad: {}".format((a+b)/2))
+# print("Tamrin #2")
+# a = int(input("Adad aval ro vared konid: "))
+# b = int(input("Adad dovom ro vared konid: "))
+# print("Jam 2 adad mishavad: {}".format((a+b)/2))
 
-print("Tamrin #3")
-a = int(input("Hoghogh khod ra vared konid: "))
-print(f"Shoma {a*0.09} maleyat va {a*0.1} hazine bime midahid")
-print(f"Hoghogh Shoma barabar ast ba {a - (a*0.19)}")
+# print("Tamrin #3")
+# a = int(input("Hoghogh khod ra vared konid: "))
+# print(f"Shoma {a*0.09} maleyat va {a*0.1} hazine bime midahid")
+# print(f"Hoghogh Shoma barabar ast ba {a - (a*0.19)}")
 
-print("Tamrin #4")
-a = int(input("Shoae dayere mored nazar ra vared konid: "))
-print(f"Masahat Dayere: {a*a*3.14} va mohit Dayere: {2*3.14*a}")
+# print("Tamrin #4")
+# a = int(input("Shoae dayere mored nazar ra vared konid: "))
+# print(f"Masahat Dayere: {a*a*3.14} va mohit Dayere: {2*3.14*a}")
 
-print('Tamrin #5')
-a = int(input('Nomre Daneshjo ra vared konid: '))
-if a > 10:
-    print('Tabrik!, Daneshjo ghabol shode ast')
-else:
-    print('Vaz kharabe!, Daneshjo Oftad')
+# print('Tamrin #5')
+# a = int(input('Nomre Daneshjo ra vared konid: '))
+# if a > 10:
+#     print('Tabrik!, Daneshjo ghabol shode ast')
+# else:
+#     print('Vaz kharabe!, Daneshjo Oftad')
